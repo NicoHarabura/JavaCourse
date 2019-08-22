dc96512e3d4806b7af088ca4c12416471b354500
NicoHarabura <nharabura@hotmail.com>
2019-08-22 17:37:27 -0300

    Comparator objeto de classe anônima

--- src/course/Teoria/ExemploLambda/application/Program.java
+++ src/course/Teoria/ExemploLambda/application/Program.java
@@ -1,6 +1,7 @@
 package course.Teoria.ExemploLambda.application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import course.Teoria.ExemploLambda.entities.Produto;
@@ -14,6 +15,13 @@ public class Program {
 		list.add(new Produto("TV", 900.00));
 		list.add(new Produto("Notebook", 1200.00));
 		list.add(new Produto("Tablet", 450.00));
+		
+		Comparator<Produto> comp = new Comparator<Produto>() {
+			@Override
+			public int compare(Produto p1, Produto p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
 		
 		list.sort(new MyComparator());
 
